3481e4655ee6205a93e0851bbfcc0c3bd1cb0e0c
Nicolas Orefice <norefice45@gmail.com>
2024-12-03 15:16:03 +0100

    api authentification

--- back-end/src/authentification/auth.rs
+++ back-end/src/authentification/auth.rs
@@ -1,20 +1,31 @@
+use std::fs;
 use serde::{Deserialize, Serialize};
 use rand::{RngCore, rngs::OsRng};
 use hex;
 use jsonwebtoken::{encode, Header, EncodingKey};
+use chrono::Utc;
 
-#[derive(Deserialize, Serialize)] 
+
+#[derive(Deserialize, Serialize, Debug, Clone)] 
 pub struct AuthData {
     pub username: String,
     pub password: String,
+    pub token: Option<String>,
+    pub token_timestamp: Option<i64>,
 }
 
 #[derive(Deserialize, Serialize)] 
 pub struct AuthAnswer {
     pub answer: bool,
-    pub token: String,
+    pub token: Option<String>,
 }
 
+
+#[derive(Deserialize, Serialize)] 
+pub struct AnswerTokenValidation {
+    pub answer: bool,
+    pub username: Option<String>,
+}
 
 #[derive(Serialize)]
 struct Claims {
@@ -39,4 +50,55 @@ pub fn generate_token(secret_key:&str, username:&str) -> String{
     let encoding_key = EncodingKey::from_secret(secret_key.as_bytes());
     let token = encode(&Header::default(),&claims, &encoding_key).unwrap();
     return token
+}
+
+
+
+pub fn verify_account(username:&str, password:&str)-> bool{
+    let json_data = fs::read_to_string("account.json").expect("Unable to access the file");
+    let user:AuthData = serde_json::from_str(&json_data).expect("Json file was not well formated");
+
+    if username == user.username && password == user.password{
+        return true;
+    }
+    else{
+        return false;
+    }
+
+}
+
+
+pub fn push_secret_key(secret_key:&str, timestamp:i64){
+    let json_data = fs::read_to_string("account.json").expect("failed to load the json file");
+    let mut user: AuthData = serde_json::from_str(&json_data).expect("failed to acces the data inside");
+    user.token = Some(secret_key.to_string());
+    user.token_timestamp = Some(timestamp);
+    let updated_json = serde_json::to_string_pretty(&user).expect("Failed to serialize updated AuthData");
+    fs::write("account.json", updated_json).expect("Failed to write updated data to JSON");
+
+}
+
+
+
+pub fn secret_key_validation(secret_key:&str) ->bool{
+    let string_secret_key = secret_key.to_string();
+    let json_data = fs::read_to_string("account.json").expect("failed to load the json file");
+    let user: AuthData = serde_json::from_str(&json_data).expect("failed to acces the data inside");
+    let timestamp_now: i64 = Utc::now().timestamp();
+    let token = Some(user.token).unwrap().unwrap();
+    let timestamp_token = Some(user.token_timestamp).unwrap().unwrap();
+    let difference = timestamp_now - timestamp_token;
+    println!("{difference}");
+    if token == string_secret_key{
+        if difference < 500{
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    else{
+        return false;
+    }
+
 }
--- back-end/src/main.rs
+++ back-end/src/main.rs
@@ -1,21 +1,57 @@
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use actix_cors::Cors;
+use chrono::Utc;
 mod authentification;
-use authentification::auth::{AuthData, AuthAnswer, generate_secret_key, generate_token};
+use authentification::auth::{generate_secret_key, generate_token, push_secret_key, secret_key_validation, verify_account, AnswerTokenValidation, AuthAnswer, AuthData};
 
 
+
+
+async fn secret_key_checking(secret_key:web::Json<AuthData>) -> impl Responder{
+    let secret_key: String = secret_key.token.clone().unwrap();  
+    let validation = secret_key_validation(&secret_key);
+    let answer_token_validation: AnswerTokenValidation;
+    if validation {
+        answer_token_validation = AnswerTokenValidation {
+            answer: true,
+            username: Some("admin".to_string()),
+    }} else{
+        answer_token_validation = AnswerTokenValidation {
+            answer: false,
+            username:None,
+            }
+        };
+    HttpResponse::Ok().json(answer_token_validation)
+
+}
 
 async fn login(auth_data: web::Json<AuthData>)->impl Responder{
-  
-    let secret_key = generate_secret_key();
-    let token = generate_token( &secret_key, &auth_data.username);
-    let _answer : AuthAnswer = AuthAnswer{
-        answer: true,
-        token : token,
-    };
-    
-    println!("Token Jnt  généré{:?}",_answer.token);
-    HttpResponse::Ok().json(_answer) // Envoie la réponse JSON
+
+    let username = auth_data.username.clone();
+    let password: String = auth_data.password.clone();
+    let answer = verify_account(&username, &password);
+    let answer_server:AuthAnswer;
+    if answer{
+        let secret_key = generate_secret_key();
+        let token = generate_token( &secret_key, &auth_data.username);
+        let token_in_json = token.clone();
+        answer_server = AuthAnswer{
+            answer: true,
+            token : Some(token),
+        };
+        let timestamp: i64 = Utc::now().timestamp(); 
+
+        push_secret_key(&token_in_json, timestamp);
+
+    }
+    else{
+        answer_server  = AuthAnswer{
+            answer: false,
+            token:None,
+        };
+
+    }
+    HttpResponse::Ok().json(answer_server) // Envoie la réponse JSON
 }
 
 #[actix_web::main]
@@ -23,7 +59,8 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .wrap(Cors::default().allow_any_origin().allow_any_method().allow_any_header())
-            .route("/auth", web::post().to(login))
+            .route("/auth/login", web::post().to(login))
+            .route("/auth/token", web::post().to(secret_key_checking))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
