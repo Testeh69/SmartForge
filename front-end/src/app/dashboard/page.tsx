980a10569b81cb64b56c1246a64a6c1bd677d70c
Nicolas Orefice <norefice45@gmail.com>
2024-12-03 18:29:11 +0100

    drag and drop made

--- front-end/src/app/dashboard/page.tsx
+++ front-end/src/app/dashboard/page.tsx
@@ -1,13 +1,15 @@
+import DragDataset from "../ui/dragDataset";
 
 
 const Dashboard = () => {
 
 
     return (
-        <div className="min-h-screen">
-            <h1>Lol</h1>
+        <div className="min-h-screen flex justify-center items-center">
+            <DragDataset/>
         </div>
     )
 }
 
 export default Dashboard;
+
